deca4320057c88fe53c890c4902f35fe37b92d73
t0xus <simon.freygang@gmail.com>
2025-12-22 20:48:27 +0100

    Ship und Battleground Generierung Implementiert

--- Battleship/Pages/Lobby.cshtml.cs
+++ Battleship/Pages/Lobby.cshtml.cs
@@ -2,11 +2,12 @@ using Battleship.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace Battleship.Pages
 {
     public class AvailableGames
-    {         
+    {
         public int MatchId { get; set; }
         public string MatchName { get; set; }
         public string HostUsername { get; set; }
@@ -24,7 +25,21 @@ namespace Battleship.Pages
     {
 
         public List<AvailableGames> AvailableGames { get; set; } = new List<AvailableGames>();
-        public List<BattleShips> BattleShips { get; set; } = new List<BattleShips>()
+        public List<BattleShips> BattleShipsHost { get; set; } = new List<BattleShips>()
+        {
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 5 }, // Carrier
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 4 }, // Battleship
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 4 }, // Battleship
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 3 }, // Destroyer
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 3 }, // Destroyer
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 3 }, // Destroyer
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 2 },  // Submarine
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 2 },  // Submarine
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 2 },  // Submarine
+            new BattleShips() { X = 0, Y = 0, across = true, boxes = 2 }  // Submarine
+        };
+
+        public List<BattleShips> BattleShipsParticipant { get; set; } = new List<BattleShips>()
         {
             new BattleShips() { X = 0, Y = 0, across = true, boxes = 5 }, // Carrier
             new BattleShips() { X = 0, Y = 0, across = true, boxes = 4 }, // Battleship
@@ -43,8 +58,8 @@ namespace Battleship.Pages
         private readonly battleshipContext _context;
         [BindProperty]
         public string HostGame { get; set; }
-
-        public string JoinMatchId { get; set;}
+        [BindProperty]
+        public string JoinMatchId { get; set; }
 
         public LobbyModel(ILogger<LobbyModel> logger, battleshipContext context)
         {
@@ -77,14 +92,14 @@ namespace Battleship.Pages
 
         public async Task<IActionResult> OnPost()
         {
-            if(!string.IsNullOrEmpty(HostGame))
+            if (!string.IsNullOrEmpty(HostGame))
             {
                 //Hole den eingelogten user
                 var userId = HttpContext.Session.GetInt32("UserId");
                 var user = await _context.Users.FindAsync(userId);
 
                 //Erstelle ein neues Spiel
-                var match = new Match();
+                var match = new Battleship.Models.Match();
                 match.MatchName = HostGame;
                 match.IdHost = user.Id;
                 match.IdParticipant = null;
@@ -96,7 +111,7 @@ namespace Battleship.Pages
                 return Page();
 
             }
-            if(!string.IsNullOrEmpty(JoinMatchId))
+            if (!string.IsNullOrEmpty(JoinMatchId))
             {
                 //Hole den eingelogten user
                 var userId = HttpContext.Session.GetInt32("UserId");
@@ -105,7 +120,7 @@ namespace Battleship.Pages
                 //Finde das Spiel
                 int matchId = int.Parse(JoinMatchId);
                 var match = await _context.Matches.FindAsync(matchId);
-                if(match != null && match.IdParticipant == null && match.Done == false)
+                if (match != null && match.IdParticipant == null && match.Done == false)
                 {
                     match.IdParticipant = user.Id;
                     _context.Matches.Update(match);
@@ -115,7 +130,7 @@ namespace Battleship.Pages
                 //Eine zufällige Zahl zwischen 0 und 1 generieren
                 System.Random rand = new System.Random();
                 int rand_turn = rand.Next(0, 2);
-                if(rand_turn == 0)
+                if (rand_turn == 0)
                 {
                     match.TurnHost = false;
                 }
@@ -127,38 +142,237 @@ namespace Battleship.Pages
                 _context.Matches.Update(match);
                 await _context.SaveChangesAsync();
 
-                createBattleShips();
-
+                BattleShipsHost = createBattleShips(BattleShipsHost);
+                BattleShipsParticipant = createBattleShips(BattleShipsParticipant);
+                
+                saveBattleShips(BattleShipsHost, match.Id, true);
+                saveBattleShips(BattleShipsParticipant, match.Id, false);
             }
 
             OnGet();
             return Page();
         }
 
-        private void createBattleShips()
+        private List<BattleShips> createBattleShips(List<BattleShips> BattleShips)
         {
             System.Random rand = new System.Random();
 
             for (int i = 0; i < BattleShips.Count; i++)
             {
-                BattleShips[i].X = rand.Next(1, 11);
-                BattleShips[i].Y = rand.Next(1, 11);
 
-                //Zufällig ausrichten
-                int across = rand.Next(0, 2);
-                if (across == 0)
+                do
                 {
-                    BattleShips[i].across = false;
-                }
-                else
-                {
-                    BattleShips[i].across = true;
-                }
+                    BattleShips[i].X = rand.Next(1, 11);
+                    BattleShips[i].Y = rand.Next(1, 11);
+
+                    //Zufällig ausrichten
+                    int across = rand.Next(0, 2);
+                    if (across == 0)
+                    {
+                        BattleShips[i].across = false;
+                    }
+                    else
+                    {
+                        BattleShips[i].across = true;
+                    }
+                } while ((BattleShips[i].across && (BattleShips[i].X + BattleShips[i].boxes - 1 > 10)) ||
+                         (!BattleShips[i].across && (BattleShips[i].Y + BattleShips[i].boxes - 1 > 10)));
+
 
                 for (int j = 0; j < i; j++)
                 {
-                    
+                    for (int k = 0; k < BattleShips[i].boxes; k++)
+                    {
+
+                        int x1 = 0;
+                        int y1 = 0;
+                        int x2 = 0;
+                        int y2 = 0;
+
+
+
+                        for (int l = 0; l < BattleShips[j].boxes; l++)
+                        {
+
+
+                            if (BattleShips[i].across)
+                            {
+                                x1 = BattleShips[i].X + k;
+                                y1 = BattleShips[i].Y;
+                            }
+                            else
+                            {
+                                x1 = BattleShips[i].X;
+                                y1 = BattleShips[i].Y + k;
+                            }
+
+                            if (BattleShips[j].across)
+                            {
+                                x2 = BattleShips[j].X + l;
+                                y2 = BattleShips[j].Y;
+                            }
+                            else
+                            {
+                                x2 = BattleShips[j].X;
+                                y2 = BattleShips[j].Y + l;
+                            }
+
+                            if ((x1 == x2 && y1 == y2))
+                            {
+                                do
+                                {
+                                    //Kollision, neu generieren
+                                    BattleShips[j].X = rand.Next(1, 11);
+                                    BattleShips[j].Y = rand.Next(1, 11);
+
+                                    //Zufällig ausrichten
+                                    int across_new = rand.Next(0, 2);
+                                    if (across_new == 0)
+                                    {
+                                        BattleShips[j].across = false;
+                                    }
+                                    else
+                                    {
+                                        BattleShips[j].across = true;
+                                    }
+                                } while ((BattleShips[j].across && (BattleShips[j].X + BattleShips[j].boxes - 1 > 10)) ||
+                                         (!BattleShips[j].across && (BattleShips[j].Y + BattleShips[j].boxes - 1 > 10)));
+                                //Setze j zurück, um erneut zu prüfen
+                                j = -1;
+                                break;
+                            }
+                        }
+
+                        if (x1 == x2 && y1 == y2)
+                        {
+                            break;
+                        }
+                    }
                 }
+            }
+        
+            return BattleShips;
+        }
+
+        private void saveBattleShips(List<BattleShips> BattleShips, int match_id, Boolean field_host)
+        {
+            //Speichere die Schiffe in der Datenbank
+            for (int i = 1; i <= 10; i++)
+            {
+                var battleground_raw = new Battleground();
+
+                battleground_raw.IdMatch = match_id;
+                battleground_raw.FieldHost = field_host;
+                battleground_raw.YCord = i;
+                battleground_raw.FieldA = 0;
+                battleground_raw.FieldB = 0;
+                battleground_raw.FieldC = 0;
+                battleground_raw.FieldD = 0;
+                battleground_raw.FieldE = 0;
+                battleground_raw.FieldF = 0;
+                battleground_raw.FieldG = 0;
+                battleground_raw.FieldH = 0;
+                battleground_raw.FieldI = 0;
+                battleground_raw.FieldJ = 0;
+
+
+                _context.Battlegrounds.Add(battleground_raw);
+            }
+
+            _context.SaveChanges();
+
+            foreach (var ship in BattleShips)
+            {
+                if (ship.across)
+                {
+                    var battleground_line = _context.Battlegrounds.FirstOrDefault(b => b.IdMatch == match_id && b.YCord == ship.Y && b.FieldHost == field_host);
+
+                    for (int i = 0; i < ship.boxes; i++)
+                    {
+                        switch (ship.X + i)
+                        {
+                            case 1:
+                                battleground_line.FieldA = 2;
+                                break;
+                            case 2:
+                                battleground_line.FieldB = 2;
+                                break;
+                            case 3:
+                                battleground_line.FieldC = 2;
+                                break;
+                            case 4:
+                                battleground_line.FieldD = 2;
+                                break;
+                            case 5:
+                                battleground_line.FieldE = 2;
+                                break;
+                            case 6:
+                                battleground_line.FieldF = 2;
+                                break;
+                            case 7:
+                                battleground_line.FieldG = 2;
+                                break;
+                            case 8:
+                                battleground_line.FieldH = 2;
+                                break;
+                            case 9:
+                                battleground_line.FieldI = 2;
+                                break;
+                            case 10:
+                                battleground_line.FieldJ = 2;
+                                break;
+                        }
+                    }
+
+                    _context.Battlegrounds.Update(battleground_line);
+
+                }
+                else
+                {
+                    for (int i = ship.Y; i < ship.Y + ship.boxes; i++)
+                    {
+                        var battleground_line = _context.Battlegrounds.FirstOrDefault(b => b.IdMatch == match_id && b.YCord == i && b.FieldHost == field_host);
+
+                        switch (ship.X)
+                        {
+                            case 1:
+                                battleground_line.FieldA = 2;
+                                break;
+                            case 2:
+                                battleground_line.FieldB = 2;
+                                break;
+                            case 3:
+                                battleground_line.FieldC = 2;
+                                break;
+                            case 4:
+                                battleground_line.FieldD = 2;
+                                break;
+                            case 5:
+                                battleground_line.FieldE = 2;
+                                break;
+                            case 6:
+                                battleground_line.FieldF = 2;
+                                break;
+                            case 7:
+                                battleground_line.FieldG = 2;
+                                break;
+                            case 8:
+                                battleground_line.FieldH = 2;
+                                break;
+                            case 9:
+                                battleground_line.FieldI = 2;
+                                break;
+                            case 10:
+                                battleground_line.FieldJ = 2;
+                                break;
+                        }
+
+                        _context.Battlegrounds.Update(battleground_line);
+                    }
+                }
+
+                _context.SaveChanges();
+
             }
         }
     }
--- Battleship/obj/Debug/net8.0/Battleship.AssemblyInfo.cs
+++ Battleship/obj/Debug/net8.0/Battleship.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99f44e46ec207377ee77622c7eac9e4da114864")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
